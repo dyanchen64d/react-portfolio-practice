2f2a89edda417a8516284fca32a5e673110650f6
chen.zhang <zhangchen64d@gmail.com>
2023-02-08 10:13:28 +0900

    update min width

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import Header from "./components/Header";
-import LandingSection from "./components/LandingSection";
-import ProjectsSection from "./components/ProjectsSection";
-import ContactMeSection from "./components/ContactMeSection";
-import Footer from "./components/Footer";
-import { AlertProvider } from "./context/alertContext";
-import Alert from "./components/Alert";
+import { ChakraProvider } from '@chakra-ui/react';
+import Header from './components/Header';
+import LandingSection from './components/LandingSection';
+import ProjectsSection from './components/ProjectsSection';
+import ContactMeSection from './components/ContactMeSection';
+import Footer from './components/Footer';
+import { AlertProvider } from './context/alertContext';
+import Alert from './components/Alert';
 
 function App() {
   return (
     <ChakraProvider>
       <AlertProvider>
-        <main>
+        <main style={{ minWidth: 1024 }}>
           <Header />
           <LandingSection />
           <ProjectsSection />
